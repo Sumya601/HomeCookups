df5de8e2d4b49b77df392812c93dc0ab673450fe
Sumya Akter <18101029@uap-bd.edu>
2020-09-21 16:18:54 +0600

    change

--- OrderManagement/models.py
+++ OrderManagement/models.py
@@ -3,11 +3,16 @@ from FoodManagement.models import Food
 from MerchantManagement.models import Merchant
 from CustomerManagement.models import Customer
 
+
 # Create your models here.
 class Order(models.Model):
     Order_Type = models.CharField(max_length=100)
-    Order_Status= models.CharField(max_length=200)
+    Order_Status = models.CharField(max_length=200)
 
-    food = models.ForeignKey(Food,on_delete=models.SET_NULL,null=True)
-    merchant = models.ForeignKey(Merchant,on_delete=models.SET_NULL,null=True)
-    customer = models.ForeignKey(Customer,on_delete=models.SET_NULL,null=True)
+    food = models.ForeignKey(Food, on_delete=models.SET_NULL, null=True)
+    merchant = models.ForeignKey(Merchant, on_delete=models.SET_NULL, null=True)
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+
+
+def __str__(self):
+    return self.Order_Type
